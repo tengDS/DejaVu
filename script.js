5d4c43c330088bf7f222398a94fcd0f1254ec281
tomeng70 <tomeng70@users.noreply.github.com>
2022-11-10 11:07:34 -0500

    Mr. Eng's changes to fix left and right image selection feature for homepage.html.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const image_input = document.querySelector("#image-input");
-image_input.addEventListener("change", function() {
+const image_input_left = document.querySelector("#image-input-left");
+image_input_left.addEventListener("change", function () {
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     const uploaded_image = reader.result;
-    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+    document.querySelector(
+      "#display-image-left"
+    ).style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
+
+const image_input_right = document.querySelector("#image-input-right");
+image_input_right.addEventListener("change", function () {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector(
+      "#display-image-right"
+    ).style.backgroundImage = `url(${uploaded_image})`;
   });
   reader.readAsDataURL(this.files[0]);
 });
